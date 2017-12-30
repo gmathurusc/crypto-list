48b963dec9d51d688ad666dfe4d203f9a98cdbb3
GAURAV <gmathur@usc.edu>
2017-12-30 15:04:59 -0800

    Changing checkbox style

--- app/public/js/bootstrap-table-multiple-sort.js
+++ app/public/js/bootstrap-table-multiple-sort.js
@@ -354,9 +354,15 @@
             $multiSortName.append('<option value="' + column.field + '">' + column.title + '</option>');
 
             if(!isHideShowColumnsDivAppended) {
-                $hideShowColumns.append('<div class="pretty p-default p-round p-thick col-md-5 col-sm-6 col-xs-12" style="margin-top:5px;margin-bottom:5px;">' +
-                    '<input type="checkbox" class="toggle-columns" id="' + column.field + '" checked />' +
-                    '<div class="state p-primary-o"><label>' + column.title + '</label></div>');
+                $hideShowColumns.append('<div class="pretty p-svg p-curve col-md-5 col-sm-6 col-xs-12" style="margin-top:5px;margin-bottom:5px; color: #1F2739">'+
+                    '<input type="checkbox" id="' + column.field + '" checked/>'+
+                    '   <div class="state p-success">'+
+                    '    <svg class="svg svg-icon" viewBox="0 0 20 20">'+
+                    '       <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>'+
+                    '   </svg>'+
+                    '   <label>' + column.title + '</label>'+
+                    '</div>'+
+                    '</div>');
             }
         });
         isHideShowColumnsDivAppended = true;

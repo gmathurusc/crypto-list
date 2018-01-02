c0cfe869d9c7a3de5974d52c24fd4686fcf03b62
GAURAV <gmathur@usc.edu>
2018-01-02 04:05:43 -0800

    Fix sticky header

---
+++ app/public/js/responsive.js
@@ -0,0 +1,24 @@
+$( window ).resize(function() {
+    windowSizeActions();
+});
+
+function windowSizeActions() {
+    if ($(window).width() < 800) {
+        $('.modal-content').css("width","100%");
+        $('#table').addClass('small-font');
+        $('th.optional').each(function () {
+            var field = $(this).data('field');
+            $table.bootstrapTable('hideColumn', field);
+        });
+        $('#hide-show-columns').addClass('hide-mobile');
+
+    }
+
+    else {
+        $('.modal-content').css("width","120%");
+        $('#table').removeClass('small-font');
+
+        $table.bootstrapTable('showAllColumns');
+        $('#hide-show-columns').removeClass('hide-mobile');
+    }
+}

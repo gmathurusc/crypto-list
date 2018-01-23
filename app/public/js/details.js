5771b476e0b2a0af8c16d0ed0bfbb82c98d1a350
Gaurav Mathur <gmathur@usc.edu>
2018-01-23 00:20:03 -0800

    Modifying graph

--- app/public/js/details.js
+++ app/public/js/details.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
 });
 
 function getHistory(symbol, day) {
+    if($('#charts').css('display') == 'none')
+        $('#container-loading').show();
     $.ajax({
         url: '/currency/history/?value='+symbol+'&day='+day,
         type: 'GET',
@@ -17,9 +19,11 @@ function getHistory(symbol, day) {
                 var marketCap = 'market_cap' in json ? json['market_cap'] : '';
                 var price = 'price' in json ? json['price'] : '';
                 var volume = 'volume' in json ? json['volume'] : '';
-                if(price !== '') googleChart('price', price);
-                if(volume !== '') googleChart('market-cap', marketCap);
-                if(volume !== '') googleChart('volume', volume);
+                if(price !== '') canvasCharts('price', price);
+                if(volume !== '') canvasCharts('market-cap', marketCap);
+                if(volume !== '') canvasCharts('volume', volume);
+                $('#charts').show();
+                $('#container-loading').hide();
             }
         },
         error: function(e) {
@@ -70,4 +74,67 @@ function googleChart(type, json) {
         var chart = new google.visualization.LineChart(document.getElementById(type+'-chart'));
         chart.draw(data, options);
     }
+}
+
+function canvasCharts(option, json) {
+    var xAxisData = [];
+    var yAxisData = [];
+    for (var i = 0; i < json.length; i++) {
+        var xAxis = new Date(json[i][0]);
+        var yAxis = option === 'price' ? json[i][1] : json[i][1] / 1000000000;
+        xAxisData.push(xAxis);
+        yAxisData.push(yAxis);
+    }
+    var label = option === 'price' ? toTitleCase(option.replace('-', ' ')) + ' ( $ ) ' : toTitleCase(option.replace('-', ' ') + ' ( $ in Billions )');
+
+    var chart = {
+        labels: xAxisData,
+        datasets : [
+            {
+                label: label,
+                borderColor: 'green',
+                data: yAxisData,
+                fill: false
+            }
+        ]
+    };
+
+    var itemNode = document.getElementById(option+'-chart');
+    itemNode.parentNode.removeChild(itemNode);
+    document.getElementById(option+'-chart-div').innerHTML = '<canvas id="'+option+'-chart"></canvas>';
+    var ctx = document.getElementById(option+'-chart').getContext("2d");
+
+    new Chart(ctx, {
+        type: 'line',
+        data: chart,
+        fill: false,
+        options: {
+            elements: { point: { radius: 0 } },
+            legend: {
+                labels: {
+                    fontColor: "black",
+                    fontSize: 18
+                }
+            },
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    ticks: {
+                        fontColor: "black",
+                        fontSize: 18
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        fontColor: "black",
+                        fontSize: 18
+                    }
+                }]
+            },
+            tooltips: {
+                titleFontSize: 14,
+                bodyFontSize: 18
+            }
+        }
+    });
 }
